8b3daef94f163fed3697f3f902b0a1c232e8f4e5
Nick <nick.deniel.em@gmail.com>
2020-08-04 12:44:33 +0300

    added keywords to main template

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,7 +1,54 @@
 @extends('layouts.main')
 
+@section('head')
+
+@endsection
+
 @section('title-block', 'Главная')
 
+
 @section('content')
-    <p>Главная страница</p>
+    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+            <li data-target="#myCarousel" data-slide-to="1" class=""></li>
+        </ol>
+
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <svg class="bd-placeholder-img" xmlns="http://www.w3.org/2000/svg"
+                     preserveAspectRatio="xMidYMid slice" focusable="false" role="img">
+                    <rect width="100%" height="100%" fill="#777"></rect>
+                </svg>
+
+                <div class="container">
+                    <div class="carousel-caption text-left">
+                        <img src="{{ URL::asset('/storage/home1-slide1.jpg') }}" width="100%">
+                    </div>
+                </div>
+            </div>
+
+            <div class="carousel-item">
+                <svg class="bd-placeholder-img" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img"><rect width="100%" height="100%" fill="#777"></rect></svg>
+                <div class="container">
+                    <div class="carousel-caption">
+                        <h1>Another example headline.</h1>
+                        <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+                        <p><a class="btn btn-lg btn-primary" href="#" role="button">Learn more</a></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <a class="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#myCarousel" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+
+    </div>
+
 @endsection
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,13 +5,18 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <meta name="description" content="Постоянно в наличии огромный выбор качественных оригинальных и неоригинальных, б/у и новых запчастей для автомобилей марки ВАЗ ЗАЗ Daewoo ДЕО ГАЗ Таврия Москвич">
+    <meta name="keywords" content="авторазборка Daewoo, авторазборка ДЕО, авторазборка ГАЗ, авторазборка ВАЗ, авторазборка ЗАЗ, авторазборка Таврия, авторазборка Москвич,
+   разборка Daewoo, разборка ДЕО, разборка ГАЗ, разборка ВАЗ, разборка ЗАЗ, разборка Таврия, разборка Москвич,
+   детали Daewoo, детали ДЕО, детали ГАЗ, детали ВАЗ, детали ЗАЗ, детали Таврия, детали Москвич,
+   б/у запчасти Daewoo, б/у запчасти ДЕО, б/у запчасти ГАЗ, б/у запчасти ВАЗ, б/у запчасти ЗАЗ, б/у запчасти Таврия, б/у запчасти Москвич,
+   б/у резина" />
+
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
 
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"
-            integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI"
-            crossorigin="anonymous"></script>
-
+    @yield('head')
     <title> Авторазборка Запорожье - @yield('title-block')</title>
 </head>
 <body class="bg-light">
@@ -31,5 +36,8 @@
             @yield('content')
         </div>
     </div>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </body>
 </html>
